a61ead7d722a5a1e323fb303fc8c8d8507a7b3fe
Kevin Morris <kevin.d.morris4@gmail.com>
2024-06-04 17:27:06 -0400

    Clean up abs module

--- src/arithmetic/abs/abs.ts
+++ src/arithmetic/abs/abs.ts
@@ -3,7 +3,7 @@
  *
  * @since 0.5.0
  * @param {number} x The number.
- * @returns {number|undefined} Returns the absolute value of of `x`.
+ * @returns {number} Returns the absolute value of of `x`.
  *
  * @example
  * abs(-2)
