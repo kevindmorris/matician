b9390a894c8b0e486f76860b35462fdd53236d90
Kevin Morris <kevin.d.morris4@gmail.com>
2024-06-05 20:50:13 -0400

    Sunset sumBy

--- src/arithmetic/sum/sum.ts
+++ src/arithmetic/sum/sum.ts
@@ -1,18 +1,32 @@
 /**
- * Compute the sum of numbers in an array.
+ * Compute the sum of an array.
  *
  * @since 0.1.0
- * @param {Array} array An array of numbers.
+ * @param {Array} array An array.
+ * @param {Function} [iteratee] The iteratee invoked on each element.
  * @returns {number} Returns the sum.
  *
  * @example
- * sum([1,2,3])
+ * sum([1, 2, 3])
  * // => 6
+ *
+ * const objects = [{ 'n': 4 }, { 'n': 2 }, { 'n': 8 }, { 'n': 6 }]
+ * sumBy(objects, ({ n }) => n)
+ * // => 20
  */
-export default function sum(array: number[]): number {
-  if (array === null || array.length === 0) return 0;
+export default function sum(
+  array: any[],
+  iteratee?: (o: any) => number
+): number {
+  let sum: number = NaN;
 
-  return array.reduce((prev, curr) => {
-    return prev + curr;
-  }, 0);
+  for (let i = 0; i < array.length; i++) {
+    const element = iteratee ? iteratee(array[i]) : array[i];
+
+    if (typeof element === "number") {
+      sum = Number.isNaN(sum) ? element : sum + element;
+    }
+  }
+
+  return sum;
 }
