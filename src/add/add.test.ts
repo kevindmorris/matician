f1a8fd0033b7106d9d9bb67a1eb97c89a0f8076e
Kevin Morris <kevin.d.morris4@gmail.com>
2025-07-07 12:08:46 -0400

    Improve add support

--- src/add/add.test.ts
+++ src/add/add.test.ts
@@ -14,6 +14,12 @@ describe("add", () => {
     expect(add(-2, -3)).toBe(-5);
   });
 
+  it("adds more than two numbers", () => {
+    expect(add(1, 2, 3)).toBe(6);
+    expect(add(1, 2, 3, 4, 5)).toBe(15);
+    expect(add(1, 2, 3, 4, -5)).toBe(5);
+  });
+
   it("adds zero correctly", () => {
     expect(add(0, 0)).toBe(0);
     expect(add(0, 5)).toBe(5);
--- src/add/add.ts
+++ src/add/add.ts
@@ -1,16 +1,24 @@
 /**
- * Adds two numbers together.
+ * Adds two or more numbers together.
  *
  * @param augend - The number to which another number (the addend) is added.
  * @param addend - The number that is added to the augend.
- * @returns The sum of the `augend` and `addend`.
+ * @param {...number} args - Additional numbers to include in the sum.
+ * @returns The sum of the `augend`, `addend`, and additional numbers.
  *
  * @example
  * ```ts
  * add(2, 3); // returns 5
  * add(-1, 1); // returns 0
+ * add(1, 2, 3, 4); // returns 10
  * ```
  */
-export default function add(augend: number, addend: number): number {
-  return augend + addend;
+export default function add(
+  augend: number,
+  addend: number,
+  ...args: number[]
+): number {
+  return args.reduce((prev, curr) => {
+    return prev + curr;
+  }, augend + addend);
 }
